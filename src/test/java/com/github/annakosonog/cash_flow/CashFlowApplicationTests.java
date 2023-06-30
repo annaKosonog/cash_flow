dd6e5028252060e032488202f612946655d73582
annaKosonog <anna.kosonog21@gmail.com>
2023-06-30 21:55:57 +0200

    First commit

---
+++ src/test/java/com/github/annakosonog/cash_flow/CashFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.annakosonog.cash_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
