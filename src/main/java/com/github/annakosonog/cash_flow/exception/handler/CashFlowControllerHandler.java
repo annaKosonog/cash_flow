e633b2d59d33327387441ead41780c1453588b7e
annaKosonog <anna.kosonog21@gmail.com>
2023-07-03 21:39:15 +0200

    Adding a method to handle errors on the controller side

---
+++ src/main/java/com/github/annakosonog/cash_flow/exception/handler/CashFlowControllerHandler.java
@@ -0,0 +1,18 @@
+package com.github.annakosonog.cash_flow.exception.handler;
+
+import com.github.annakosonog.cash_flow.exception.CashFlowException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class CashFlowControllerHandler {
+
+    @ExceptionHandler(value = CashFlowException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> cashFlowExceptionResponseError(CashFlowException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
